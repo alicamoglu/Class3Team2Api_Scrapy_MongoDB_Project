79c35fac5623de0301ed4deb691db5434421e088
Adem DOĞAN <ademfatihdogan@gmail.com>
2023-03-24 13:10:16 +0100

    filled the ui labels
    I added two rows to table for test

--- pyqt5/weather_main.py
+++ pyqt5/weather_main.py
@@ -9,28 +9,31 @@ class Main_Class(QMainWindow,  Ui_MainWindow):
     def __init__(self):
         super(Main_Class, self).__init__()
         self.setupUi(self)
-        self.comboBox_city.currentTextChanged.connect(self.get_weather)
+        self.table_cities.cellClicked.connect(self.get_weather)
     
-    
-    def get_weather(self,city_name):
+    def get_weather(self, row, column):
         
-
+        current_row = self.table_cities.currentRow()
+        current_column = self.table_cities.currentColumn()
+        city_name = self.table_cities.item(current_row, current_column).text()
+               
         api_key = '1c50e484391dc9fbbaa60f8c4ef4c22b'
-
-        #city_name = input("City Name")
         weather_data = requests.get(
             f"https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={api_key}&units=metric")
 
-        #    f"https://api.openweathermap.org/data/2.5/weather?q={city_name}&units=imperial&APPID={api_key}")
-
-        print(weather_data.json())
+#parse json data
+        # print(weather_data.json())
         weather = weather_data.json()['weather'][0]['main']
         temp = round(weather_data.json()['main']['temp'])
         humidity = weather_data.json()['main']['humidity']
-        # resilince =
         wind_speed = round(weather_data.json()['wind']['speed'],1)
+        pressure = weather_data.json()['main']['pressure']
 
-        print(weather, temp, humidity,wind_speed)
+#fill the ui label
+        self.label_temperature.setText(str(temp)+"^C")
+        self.label_huminity.setText(str(humidity))
+        self.label_wind.setText(str(wind_speed))
+        self.label_pressure.setText(str(pressure))
 
 
 if __name__ == "__main__":
