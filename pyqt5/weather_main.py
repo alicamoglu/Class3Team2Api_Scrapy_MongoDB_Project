e42f996a12a4c299ce6afdffddf96be99dfe7a76
Adem DOĞAN <ademfatihdogan@gmail.com>
2023-03-24 13:58:11 +0100

    weather data saved mongodb
    I put a country for test

--- pyqt5/weather_main.py
+++ pyqt5/weather_main.py
@@ -1,6 +1,7 @@
 from PyQt5.QtWidgets import *
 import sys
 import requests
+import pymongo
 
 
 from Ui_weather_proje import *
@@ -10,9 +11,13 @@ class Main_Class(QMainWindow,  Ui_MainWindow):
         super(Main_Class, self).__init__()
         self.setupUi(self)
         self.table_cities.cellClicked.connect(self.get_weather)
+
+        
+        self.client = pymongo.MongoClient("mongodb+srv://sumeyra:1234@cluster0.rvan9sx.mongodb.net/?retryWrites=true&w=majority")
+        self.db = self.client["weather_app"]
+        self.collection = self.db["weather_infos"]
     
     def get_weather(self, row, column):
-        
         current_row = self.table_cities.currentRow()
         current_column = self.table_cities.currentColumn()
         city_name = self.table_cities.item(current_row, current_column).text()
@@ -34,6 +39,21 @@ class Main_Class(QMainWindow,  Ui_MainWindow):
         self.label_huminity.setText(str(humidity))
         self.label_wind.setText(str(wind_speed))
         self.label_pressure.setText(str(pressure))
+
+# #insert to mongodb database
+        item = {
+            "country" : self.label_country_info.text(),
+            "city_name" : city_name,
+            "temperature" : temp,
+            "humidity" : humidity,
+            "wind_speed" : wind_speed,
+            "pressure" : pressure
+        }
+
+        try:
+            self.collection.insert_one(item)
+        except pymongo.errors.WriteError as e:
+            print("Save Error : ", e)
 
 
 if __name__ == "__main__":
