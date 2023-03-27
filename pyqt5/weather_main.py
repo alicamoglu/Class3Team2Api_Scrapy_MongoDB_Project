cfe6e44df13b1c6ac3c66958a643d8315cc680c5
Nuseybe <nuseybekarakurt@gmail.com>
2023-03-27 11:16:34 +0200

    update by Nuseybe

--- pyqt5/weather_main.py
+++ pyqt5/weather_main.py
@@ -23,6 +23,7 @@ class Main_Class(QMainWindow,  Ui_MainWindow):
         self.table_cities.itemSelectionChanged.connect(self.get_city_info_germany)
         self.table_cities.itemSelectionChanged.connect(self.get_city_info_netherland)
         self.table_cities.itemSelectionChanged.connect(self.get_city_info_usa)
+        self.Button_find.clicked.connect(self.search_city)
         
     def get_weather(self, row, column):
         current_row = self.table_cities.currentRow()
@@ -172,6 +173,70 @@ class Main_Class(QMainWindow,  Ui_MainWindow):
         self.label_country_info.setText("USA")
         self.label_region_info.setText(city_info["region"])
         self.label_population_info.setText(str(city_info["population"]))    
+    
+    def search_city(self):
+        self.city = self.lineEdit_city.text()
+        if len(self.city) == 0 :
+            return
+        
+        myquery = {"city": self.city}
+        search_city_german= self.city_germany.find(myquery)
+        search_city_netherland = self.city_netherland.find(myquery)
+        search_city_america = self.city_america.find(myquery)
+        for x in search_city_german:
+            self.label_country_info.setText("Germany")
+            self.label_region_info.setText(x["region"])
+            self.label_population_info.setText(str(x["population"])) 
+            
+        for x in search_city_netherland:
+            self.label_country_info.setText("Netherland")
+            self.label_region_info.setText(x["region"])
+            self.label_population_info.setText(str(x["population"]))
+                   
+        for x in search_city_america:
+            self.label_country_info.setText("USA")
+            self.label_region_info.setText(x["region"])
+            self.label_population_info.setText(str(x["population"]))
+            
+       
+        self.label_city_name_show.setText(self.city) 
+        api_key = '1c50e484391dc9fbbaa60f8c4ef4c22b'
+        weather_data = requests.get(
+            f"https://api.openweathermap.org/data/2.5/weather?q={self.city}&appid={api_key}&units=metric")
+        
+#parse json data
+        # print(weather_data.json())
+        weather = weather_data.json()['weather'][0]['main']
+        temp = round(weather_data.json()['main']['temp'])
+        humidity = weather_data.json()['main']['humidity']
+        wind_speed = round(weather_data.json()['wind']['speed'],1)
+        pressure = weather_data.json()['main']['pressure']
+        icon = weather_data.json()['weather'][0]['icon']
+        print(weather_data.json())                            # here content of the weather_data is seen in console to nevigate for target data
+        print("-------------------------")                   # seperator
+        print(icon)                                          # here for check in console if it brings accurate weather situation icon
+#fill the ui label
+        self.label_temperature.setText(str(temp)+"°C")
+        self.label_huminity.setText(str(humidity)+"%")
+        self.label_wind.setText(str(wind_speed)+" km/h")
+        self.label_pressure.setText(str(pressure)+" mb")
+        self.label_icon_situation.setPixmap(QtGui.QPixmap(f":/newPrefix/{icon}.png"))     #label_icon_situation is send here
+
+# #insert to mongodb database
+        item = {
+            "country" : self.label_country_info.text(),
+            "city_name" : self.city,
+            "temperature" : temp,
+            "humidity" : humidity,
+            "wind_speed" : wind_speed,
+            "pressure" : pressure
+        }
+
+        try:
+            self.collection.insert_one(item)
+        except pymongo.errors.WriteError as e:
+            print("Save Error : ", e)     
+        
         
 
 if __name__ == "__main__":
