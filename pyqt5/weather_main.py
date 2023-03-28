7af0adeb16dbc3e995dfcca5e6dec2ef95806e8d
Nuseybe <113981173+Nueseybe@users.noreply.github.com>
2023-03-28 23:10:25 +0200

    update by Nuseybe

--- pyqt5/weather_main.py
+++ pyqt5/weather_main.py
@@ -1,8 +1,12 @@
+import datetime
 from PyQt5.QtWidgets import *
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtGui import QMovie
 import sys
 import requests, pymongo
 from pymongo import *
 from Ui_weather_proje import *
+from PyQt5.QtCore import QDateTime, Qt
 
 # rows_data = []
 
@@ -10,7 +14,7 @@ class Main_Class(QMainWindow,  Ui_MainWindow):
     def __init__(self):
         super(Main_Class, self).__init__()
         self.setupUi(self)
-        
+        self.label_gif.setFixedSize(100, 100)
         
         self.client = pymongo.MongoClient("mongodb+srv://sumeyra:1234@cluster0.rvan9sx.mongodb.net/?retryWrites=true&w=majority")
         self.db = self.client["weather_app"]
@@ -18,7 +22,13 @@ class Main_Class(QMainWindow,  Ui_MainWindow):
         self.city_germany = self.db["germany"]
         self.city_america = self.db["america"]
         self.city_netherland = self.db["netherland"]
-    
+        self.movie = QtGui.QMovie("world.gif")
+        self.movie.setScaledSize(QtCore.QSize(100, 100))
+        self.label_gif.setMovie(self.movie)
+        self.table_cities.setColumnWidth(0,150)
+        self.table_cities.setColumnWidth(1,120)
+        self.table_cities.setColumnWidth(2,170)
+        
         self.table_cities.cellClicked.connect(self.get_weather)
         self.comboBox_country.currentTextChanged.connect(self.get_cities)
         self.table_cities.itemSelectionChanged.connect(self.get_city_info_germany)
@@ -26,13 +36,15 @@ class Main_Class(QMainWindow,  Ui_MainWindow):
         self.table_cities.itemSelectionChanged.connect(self.get_city_info_usa)
         self.Button_find.clicked.connect(self.search_city)
         self.Button_filter.clicked.connect(self.filter)
+        self.label_gif.setMovie(self.movie)
+        self.movie.start()
         
     def get_weather(self, row, column):
         current_row = self.table_cities.currentRow()
         current_column = self.table_cities.currentColumn()
         city_name = self.table_cities.item(current_row, current_column).text()
 
-        # self.label_city_name_show.setText(city_name) 
+        self.label_city_name.setText(city_name) 
               
         api_key = '1c50e484391dc9fbbaa60f8c4ef4c22b'
         weather_data = requests.get(
@@ -46,16 +58,17 @@ class Main_Class(QMainWindow,  Ui_MainWindow):
         wind_speed = round(weather_data.json()['wind']['speed'],1)
         pressure = weather_data.json()['main']['pressure']
         icon = weather_data.json()['weather'][0]['icon']
+        datetime = QDateTime.currentDateTime()
         print(weather_data.json())                            # here content of the weather_data is seen in console to nevigate for target data
-        print("-------------------------")                   # seperator
-        print(icon)                                          # here for check in console if it brings accurate weather situation icon
+        #print("-------------------------")                   # seperator
+        #print(icon)                                          # here for check in console if it brings accurate weather situation icon
 #fill the ui label
         self.label_temperature.setText(str(temp)+"°C")
         self.label_huminity.setText(str(humidity)+"%")
         self.label_wind.setText(str(wind_speed)+" km/h")
         self.label_pressure.setText(str(pressure)+" mb")
         self.label_icon_situation.setPixmap(QtGui.QPixmap(f":/newPrefix/{icon}.png"))     #label_icon_situation is send here
-
+        self.label_update.setText(datetime.toString(Qt.DefaultLocaleLongDate)) # label_update is send here
 # #insert to mongodb database
         item = {
             "country" : self.label_country_info.text(),
@@ -98,7 +111,7 @@ class Main_Class(QMainWindow,  Ui_MainWindow):
             self.table_cities.setItem(row, 1, QtWidgets.QTableWidgetItem(result["region"]))
             self.table_cities.setItem(row, 2, QtWidgets.QTableWidgetItem(str(result["population"])))
             row +=1   
-            
+        self.label_source.setText("https://de.wikipedia.org/wiki/Liste_der_Gro%C3%9F-_und_Mittelst%C3%A4dte_in_Deutschland")    
        
     def get_america(self):
         global rows_data       
@@ -114,8 +127,7 @@ class Main_Class(QMainWindow,  Ui_MainWindow):
             self.table_cities.setItem(row, 1, QTableWidgetItem(result["region"]))
             self.table_cities.setItem(row, 2, QTableWidgetItem(str(result["population"])))
             row +=1  
-        #for i in self.city_america.find():
-            #print(i)
+        self.label_source.setText("https://en.wikipedia.org/wiki/List_of_United_States_cities_by_population")    
         
     def get_netherland(self):
         global rows_data
@@ -131,7 +143,9 @@ class Main_Class(QMainWindow,  Ui_MainWindow):
             self.table_cities.setItem(row, 1, QTableWidgetItem(result["region"]))
             self.table_cities.setItem(row, 2, QTableWidgetItem(str(result["population"])))
             row +=1  
-
+        self.label_source.setText("https://tr.wikipedia.org/wiki/Hollanda%27daki_%C5%9Fehirler_listesi")
+        
+        
     def filter(self):
         self.table_cities.clear()
         # data_cities = self.city_germany.find({"country" : "Germany"},{'city' :1,'region':1, 'population':1})
@@ -211,68 +225,76 @@ class Main_Class(QMainWindow,  Ui_MainWindow):
         self.label_population_info.setText(str(city_info["population"]))    
     
     def search_city(self):
+        self.label_country_info.clear()
+        self.label_region_info.clear()
+        self.label_population_info.clear()
+        self.label_source.clear()
+        self.label_city_name.clear()
         self.city = self.lineEdit_city.text()
         if len(self.city) == 0 :
             return
         
-        myquery = {"city": self.city}
-        search_city_german= self.city_germany.find(myquery)
-        search_city_netherland = self.city_netherland.find(myquery)
-        search_city_america = self.city_america.find(myquery)
+       
+        search_city_german= self.city_germany.find({"city": self.city},{"city" : 1, "region" :1, "population" :1})
+        search_city_netherland= self.city_netherland.find({"city": self.city},{"city" : 1,  "region" :1, "population" :1})
+        search_city_america= self.city_america.find({"city": self.city},{"city" : 1,  "region" :1, "population" :1})
         for x in search_city_german:
             self.label_country_info.setText("Germany")
             self.label_region_info.setText(x["region"])
-            self.label_population_info.setText(str(x["population"])) 
-            
+            self.label_population_info.setText(str(x["population"]))
+            self.label_source.setText("https://de.wikipedia.org/wiki/Liste_der_Gro%C3%9F-_und_Mittelst%C3%A4dte_in_Deutschland")
+            self.label_city_name.setText(self.city) 
         for x in search_city_netherland:
             self.label_country_info.setText("Netherland")
             self.label_region_info.setText(x["region"])
             self.label_population_info.setText(str(x["population"]))
-                   
+            self.label_city_name.setText(self.city)   
+            self.label_source.setText("https://tr.wikipedia.org/wiki/Hollanda%27daki_%C5%9Fehirler_listesi") 
         for x in search_city_america:
             self.label_country_info.setText("USA")
             self.label_region_info.setText(x["region"])
             self.label_population_info.setText(str(x["population"]))
+            self.label_city_name.setText(self.city)
+            self.label_source.setText("https://en.wikipedia.org/wiki/List_of_United_States_cities_by_population")
+        
+                  
+            api_key = '1c50e484391dc9fbbaa60f8c4ef4c22b'
+            weather_data = requests.get(f"https://api.openweathermap.org/data/2.5/weather?q={self.city}&appid={api_key}&units=metric")
             
-       
-        self.label_city_name_show.setText(self.city) 
-        api_key = '1c50e484391dc9fbbaa60f8c4ef4c22b'
-        weather_data = requests.get(
-            f"https://api.openweathermap.org/data/2.5/weather?q={self.city}&appid={api_key}&units=metric")
-        
-#parse json data
-        # print(weather_data.json())
-        weather = weather_data.json()['weather'][0]['main']
-        temp = round(weather_data.json()['main']['temp'])
-        humidity = weather_data.json()['main']['humidity']
-        wind_speed = round(weather_data.json()['wind']['speed'],1)
-        pressure = weather_data.json()['main']['pressure']
-        icon = weather_data.json()['weather'][0]['icon']
-        print(weather_data.json())                            # here content of the weather_data is seen in console to nevigate for target data
-        print("-------------------------")                   # seperator
-        print(icon)                                          # here for check in console if it brings accurate weather situation icon
-#fill the ui label
-        self.label_temperature.setText(str(temp)+"°C")
-        self.label_huminity.setText(str(humidity)+"%")
-        self.label_wind.setText(str(wind_speed)+" km/h")
-        self.label_pressure.setText(str(pressure)+" mb")
-        self.label_icon_situation.setPixmap(QtGui.QPixmap(f":/newPrefix/{icon}.png"))     #label_icon_situation is send here
+        #parse json data
+                # print(weather_data.json())
+            weather = weather_data.json()['weather'][0]['main']
+            temp = round(weather_data.json()['main']['temp'])
+            humidity = weather_data.json()['main']['humidity']
+            wind_speed = round(weather_data.json()['wind']['speed'],1)
+            pressure = weather_data.json()['main']['pressure']
+            icon = weather_data.json()['weather'][0]['icon']
+            datetime = QDateTime.currentDateTime()
+            print(weather_data.json())                            # here content of the weather_data is seen in console to nevigate for target data
+            print("-------------------------")                   # seperator
+            print(icon)                                          # here for check in console if it brings accurate weather situation icon
+    #fill the ui label
+            self.label_temperature.setText(str(temp)+"°C")
+            self.label_huminity.setText(str(humidity)+"%")
+            self.label_wind.setText(str(wind_speed)+" km/h")
+            self.label_pressure.setText(str(pressure)+" mb")
+            self.label_icon_situation.setPixmap(QtGui.QPixmap(f":/newPrefix/{icon}.png"))     #label_icon_situation is send here
+            self.label_update.setText(datetime.toString(Qt.DefaultLocaleLongDate)) # label_update is send here
+    # #insert to mongodb database
+            item = {
+                "country" : self.label_country_info.text(),
+                "city_name" : self.city,
+                "temperature" : temp,
+                "humidity" : humidity,
+                "wind_speed" : wind_speed,
+                "pressure" : pressure
+            }
 
-# #insert to mongodb database
-        item = {
-            "country" : self.label_country_info.text(),
-            "city_name" : self.city,
-            "temperature" : temp,
-            "humidity" : humidity,
-            "wind_speed" : wind_speed,
-            "pressure" : pressure
-        }
-
-        try:
-            self.collection.insert_one(item)
-        except pymongo.errors.WriteError as e:
-            print("Save Error : ", e)     
-        
+            try:
+                self.collection.insert_one(item)
+            except pymongo.errors.WriteError as e:
+                print("Save Error : ", e)     
+    
         
 
 if __name__ == "__main__":
